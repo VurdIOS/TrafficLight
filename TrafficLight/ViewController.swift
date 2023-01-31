e378060a9798da2a3810bbdfba36cc1b451b1f28
Kamal <kamalios@mail.ru>
2023-01-31 22:15:02 +0300

    Create the logic to change traffic light"s lights

--- TrafficLight/ViewController.swift
+++ TrafficLight/ViewController.swift
@@ -15,6 +15,8 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var startButton: UIButton!
     
+    var lightsPosition = [false, false, false]
+    
     
     
     override func viewDidLoad() {
@@ -25,14 +27,59 @@ class ViewController: UIViewController {
         greenLightView.layer.cornerRadius = greenLightView.frame.width / 2
         startButton.layer.cornerRadius = 8
         
-        
     }
 
     @IBAction func startButtonTapped(_ sender: UIButton) {
         startButton.setTitle("NEXT", for: .normal)
-        redLightView.alpha = 1
+        findPositionForLights(lightsPosition)
+        resultLightPosition(findIndexFor(lightsPosition))
+        
         
     }
+    
+    func findPositionForLights(_ indexes: Array<Bool>) {
+        switch lightsPosition {
+        case [false, false, false]:
+            lightsPosition = [true, false, false]
+        case [true, false, false]:
+            lightsPosition = [false, true, false]
+        case [false, true, false]:
+            lightsPosition = [false, false, true]
+        case [false, false, true]:
+            lightsPosition = [true, false, false]
+        default:
+            lightsPosition = [false, false, false]
+        }
+    }
+        
+    func findIndexFor(_ light: Array<Bool>) -> Int {
+        var position = 0
+        for index in light {
+            if index == true {
+                return position
+            } else {
+                position += 1
+            }
+        }
+        return position
+    }
+    
+    func resultLightPosition(_ position: Int) {
+        if position == 0 {
+            redLightView.alpha = 1
+            yellowLightView.alpha = 0.3
+            greenLightView.alpha = 0.3
+        } else if position == 1 {
+            redLightView.alpha = 0.3
+            yellowLightView.alpha = 1
+            greenLightView.alpha = 0.3
+        } else {
+            redLightView.alpha = 0.3
+            yellowLightView.alpha = 0.3
+            greenLightView.alpha = 1
+        }
+    }
+    
     
 }
 
